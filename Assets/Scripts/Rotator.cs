0475c9047004d01ee5ad9127566a585e0051fe88
BaggioPereira <baggio_111111@hotmail.com>
2018-03-05 20:44:48 +0000

    Ping Pong on rotator
    Previous commit was for colour boolean

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Rotator : MonoBehaviour {
 
     public float speed = 100f;
+    public float rotationCounter;
+    public bool pingpong;
     public bool reverse;
 
 	// Use this for initialization
@@ -15,9 +17,28 @@ public class Rotator : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if(!reverse)
-            transform.Rotate(0f, 0f, speed * Time.deltaTime);	
-        else if(reverse)
+        if (!pingpong)
+        {
+            Rotate();
+        }
+
+        else if(pingpong)
+        {
+            Rotate();
+            rotationCounter = transform.rotation.z;
+            if (rotationCounter < 0f)
+            {
+                reverse = !reverse;
+            }
+        }
+    }
+
+    void Rotate()
+    {
+        if (!reverse)
+            transform.Rotate(0f, 0f, speed * Time.deltaTime);
+
+        else if (reverse)
             transform.Rotate(0f, 0f, -speed * Time.deltaTime);
     }
 }
