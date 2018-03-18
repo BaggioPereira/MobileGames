2803f5f305be6ebca681046c1fad4142ea44cd17
BaggioPereira <baggio_111111@hotmail.com>
2018-03-18 13:25:48 +0000

    Pingpong game mode

---
+++ Assets/Scripts/ColourSwitch/PingPongManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongManager : MonoBehaviour {
+    PingPongMover[] walls;
+    ColourSwitchBall player;
+	// Use this for initialization
+	void Start ()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Time.timeScale = 0;
+        Screen.orientation = UnityEngine.ScreenOrientation.Portrait;
+        walls = GameObject.FindObjectsOfType<PingPongMover>();
+        player = GameObject.FindObjectOfType<ColourSwitchBall>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (player.score > 9)
+        {
+            for (int i = 0; i < walls.Length; i++)
+            {
+                if (!walls[i].pingpong)
+                    walls[i].pingpong = true;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/ColourSwitch/PingPongMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongMover : MonoBehaviour {
+
+    public GameObject[] lines;
+    public float speed = 100f;
+    public bool pingpong;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (pingpong)
+            {
+                lines[i].transform.position = new Vector3(transform.position.x, lines[i].transform.position.y - (speed * Time.deltaTime), transform.position.z);
+                if (lines[i].transform.position.y < -9f)
+                    lines[i].transform.position = new Vector3(lines[i].transform.position.x, lines[i].transform.position.y + 20f, lines[i].transform.position.z);
+            }
+        }
+    }
+}
